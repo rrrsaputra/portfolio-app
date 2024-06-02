afdc25e29ec3bf38b6a1ee08ec061cd086cbdf52
Raka Dimas Saputra <rakadimas56@gmail.com>
2024-06-02 20:03:21 +0700

    menambahkan tampilan di home dan portfolio

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -15,7 +15,7 @@
     <div class="min-h-full">
         <x-navbar></x-navbar>
 
-        <x-header>{{ $title }}</x-header>
+        {{-- <x-header>{{ $title }}</x-header> --}}
         <main>
             <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
                 <!-- Your content -->
